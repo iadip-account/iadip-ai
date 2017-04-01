7edb0ff3a233929ba050e04c885058e7a3fa47eb
iadip-account <firulina1994@gmail.com>
2017-04-01 11:05:12 +0400

    Модель данных, привязки данных

---
+++ IADIP/ModelViewModelCost.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+
+namespace IADIP {
+    public class ModelViewModelCost : INotifyPropertyChanged {
+        protected void CostPropertyChanged(string p) {
+            if (PropertyChanged != null) {
+                PropertyChanged(this, new PropertyChangedEventArgs(p));
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private double area;
+        private double bathes;
+        private double beech;
+        private double cost;
+
+        public double Area {
+            get {
+                return area;
+            }
+            set {
+                area = value;
+                CostPropertyChanged("Area");
+            }
+        }
+
+        public double Bathes {
+            get {
+                return bathes;
+            }
+            set {
+                bathes = value;
+                CostPropertyChanged("Bathes");
+            }
+        }
+
+        public double Beech {
+            get {
+                return beech;
+            }
+            set {
+                beech = value;
+                CostPropertyChanged("Beech");
+            }
+        }
+
+        public double Cost {
+            get {
+                return cost;
+            }
+            set {
+                cost = value;
+                CostPropertyChanged("Cost");
+            }
+        }
+    }
+}
---
+++ IADIP/ModelViewModelNeuro.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+
+namespace IADIP {
+    public class ModelViewModelNeuro : INotifyPropertyChanged {
+        protected void NeuroPropertyChanged(string p) {
+            if (PropertyChanged != null) {
+                PropertyChanged(this, new PropertyChangedEventArgs(p));
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private string path;
+        private double speed;
+        private string layers;
+        private int maximum;
+        private int iterations;
+        private string training;
+        private double error;
+
+        public string Path {
+            get {
+                return path;
+            }
+            set {
+                path = value;
+                NeuroPropertyChanged("Path");
+            }
+        }
+
+        public string Layers {
+            get {
+                return layers;
+            }
+            set {
+                layers = value;
+                NeuroPropertyChanged("Layers");
+            }
+        }
+
+        public string Training {
+            get {
+                return training;
+            }
+            set {
+                training = value;
+                NeuroPropertyChanged("Training");
+            }
+        }
+
+        public double Speed {
+            get {
+                return speed;
+            }
+            set {
+                speed = value;
+                NeuroPropertyChanged("Speed");
+            }
+        }
+
+        public double Error {
+            get {
+                return error;
+            }
+            set {
+                error = value;
+                NeuroPropertyChanged("Error");
+            }
+        }
+
+        public int Maximum {
+            get {
+                return maximum;
+            }
+            set {
+                maximum = value;
+                NeuroPropertyChanged("Maximum");
+            }
+        }
+
+    }
+}
